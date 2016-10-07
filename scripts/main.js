53b3d70dce9f4ef0deb5785ff784268e2401ca47
Jonathan Senske <dungeon47@gmail.com>
2016-10-06 21:05:23 -0600

    test
    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
-var name = prompt();
-alert(name);
+var state = prompt();
+
+function add(first, second) {
+	if (state > 16) {return first+second;}
+	else {return first+second+second;}
+}
+
+alert(add(1,2));
