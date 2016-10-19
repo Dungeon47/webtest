5eb645469da413cf2dcfdbe173e8f907c6002f90
Jonathan Senske <dungeon47@gmail.com>
2016-10-18 19:43:25 -0600

    testing
    inlineHTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
-var state = prompt();
+//var state = prompt();
 
-function add(first, second) {
-	if (state > 16) {return first+second;}
-	else {return first+second+second;}
+//function add(first, second) {
+	//if (state > 16) {return first+second;}
+	//else {return first+second+second;}
+//}
+
+//alert(add(1,2));
+
+//var myList = ['apples', 'oranges', 'banans'];
+
+var myList = ['apples', 'oranges', 'bananas'];
+
+for (var i=0; i < myList.length; i++) {
+  alert('you have '+myList[i]+' in your basket.')
 }
-
-alert(add(1,2));
